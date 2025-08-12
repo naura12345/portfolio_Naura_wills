c52f5e7b9f63ef593cea8f330788594f58743424
naura12345 <naura.williamsditsougou@esprit.tn>
2025-08-12 19:37:13 +0100

    Ajout du portfolio HTML/CSS

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Bouton N8N pour Contacts',
+    'version': '14.0.1.0',
+    'summary': 'Ajoute un bouton pour envoyer des données à n8n depuis les contacts.',
+    'description': 'Ce module ajoute un bouton sur les fiches contact pour déclencher un workflow n8n.',
+    'author': 'Ton Nom',
+    'website': 'https://tonsite.exemple',
+    'category': 'Tools',
+    'depends': ['base', 'contacts'],
+    'data': ['views/res_partner_view.xml'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
